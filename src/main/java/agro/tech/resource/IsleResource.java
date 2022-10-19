e6716bfbe7b8364533294bf412ff4cc574eb377f
Nathalia Miranda <mirandanathaliaf@gmail.com>
2022-10-18 23:25:19 -0300

    feat: implement update and delete isle endpoints

--- src/main/java/agro/tech/resource/IsleResource.java
+++ src/main/java/agro/tech/resource/IsleResource.java
@@ -6,6 +6,8 @@ import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,5 +43,21 @@ public class IsleResource {
   @Produces(MediaType.APPLICATION_JSON)
   public Isle get(@PathParam("id") String id) throws NotFoundException {
     return isleService.findById(id);
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Isle update(@PathParam("id") String id, Isle isle) throws NotFoundException {
+    return isleService.update(id, isle);
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response delete(@PathParam("id") String id) throws NotFoundException {
+    isleService.delete(id);
+    return Response.status(204).build();
   }
 }
--- src/main/java/agro/tech/service/IsleService.java
+++ src/main/java/agro/tech/service/IsleService.java
@@ -9,6 +9,7 @@ import agro.tech.repository.IsleRepository;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class IsleService {
@@ -33,4 +34,21 @@ public class IsleService {
     return isle;
   }
 
+  public Isle update(String id, Isle updated) throws NotFoundException {
+    Isle isle = findById(id);
+    if (isle == null) {
+      throw new NotFoundException();
+    }
+    isle.setStatus(updated.getStatus());
+    isleRepository.update(isle);
+    return isle;
+  }
+
+  public void delete(String id) throws NotFoundException {
+    Isle isle = findById(id);
+    if (isle == null) {
+      throw new NotFoundException();
+    }
+    isleRepository.delete(isle);
+  }
 }
