4fab50bc299880bf8beebf3a2dd9f5c3b12954c5
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-10-19 21:28:51 -0300

    tests: add resource layer tests

--- src/main/java/agro/tech/service/MeasurementsService.java
+++ src/main/java/agro/tech/service/MeasurementsService.java
@@ -1,10 +1,10 @@
 package agro.tech.service;
 
+import agro.tech.model.Measurements;
+import agro.tech.repository.MeasurementsRepository;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import agro.tech.model.Measurements;
-import agro.tech.repository.MeasurementsRepository;
 
 @ApplicationScoped
 public class MeasurementsService {
